efc874de18e40bed1642159ef2a826fc6e163493
Meghana2119 <meghana_nikam@yahoo.co.uk>
2024-06-24 19:38:30 +0100

    "New res.send() added"

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+  res.send("Hi")
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+app.get('/myapi',(req,res)=>{
+    res.send("<h1>My API</h1>")
 })
