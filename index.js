5d38b699ea051154188d96e27e51fe4df2143659
Meghana2119 <meghana_nikam@yahoo.co.uk>
2024-06-24 20:01:38 +0100

    POST method

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-app.get('/myapi',(req,res)=>{
-    res.send("<h1>My API</h1>")
+   res.send('Hello World!')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+  app.get('/myapi',(req,res)=>{
+     res.send("<h1>My API</h1>")
+  })
+app.use(express.json())
+app.post('/create_user',(req,res)=>{
+    const userData=req.body
+    res.status(201).json({message:'USer created successfully',userData})
 })
